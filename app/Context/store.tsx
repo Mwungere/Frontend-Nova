45f4a1ebea85d463c2091efde8423971a9659832
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-02-05 20:52:26 +0200

    another commit

--- app/Context/store.tsx
+++ app/Context/store.tsx
@@ -1,11 +1,16 @@
 "use client"
 import React, {createContext, useContext, useState, ReactNode} from "react";
 interface DarkModeContextProps{
-    darkMode:boolean;
+  darkMode: boolean,
     toggleDarkMode:() =>void;
 }
+
+interface ChildrenProps{
+  children:ReactNode
+}
+
 const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined);
-export const DarkModeProvider= ({ children }) => {
+export const DarkModeProvider= ({ children }: ChildrenProps) => {
     const [darkMode, setDarkMode] = useState(false);
   
     const toggleDarkMode = () => {
