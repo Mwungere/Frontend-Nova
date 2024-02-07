b53ad7c853425964dc38cb1e69bb1b950ba3abdc
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-02-07 21:44:37 +0200

    how can i say?

--- components/Firebase.tsx
+++ components/Firebase.tsx
@@ -3,12 +3,15 @@ import { getAuth, Auth } from "firebase/auth"; // Import Auth type from firebase
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpBmTvKcTvLjLgo3tX94O7tjSecw350TI",
-  authDomain: "nova-ruddy.vercel.app",
+  authDomain: "social-login-and-signup-44139.firebaseapp.com",
   projectId: "social-login-and-signup-44139",
   storageBucket: "social-login-and-signup-44139.appspot.com",
   messagingSenderId: "569129685272",
   appId: "1:569129685272:web:fd055d1f48a39983070e6d",
   measurementId: "G-LY2JNY2ZSG",
+  auth: {
+    redirectUrl: "https://nova-ruddy.vercel.app", // Your redirect URL
+  },
 };
 
 let app: any;
