81994614dc170914b366ba56c92c7cf71ab30a5c
VerygoodMuhriwa <verygoodmuhriwa2@gmail.com>
2024-03-07 17:11:51 +0200

    after adding progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { DarkModeProvider } from "./Context/store";
-
+import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <DarkModeProvider>{children}</DarkModeProvider>
+        <Providers>
+          <DarkModeProvider>{children}</DarkModeProvider>
+        </Providers>
       </body>
     </html>
   );
