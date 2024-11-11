4b86ec5026cf815a6f41b8786c1947352dd0e3ac
Mwungere <119742006+Mwungere@users.noreply.github.com>
2024-11-11 14:36:55 +0200

    Update Footer.tsx

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <div className=' flex flex-col w-full h-max bg-secondary'>
             <div className=' flex w-full flex-wrap'>
                 <div className={` lg:w-1/2 flex flex-col p-10  text-white font-body`}>
-                    <h1 className={` font-body text-3xl font-bold mb-5`}>Nova</h1>
+                    <h1 className={` font-body text-3xl font-bold mb-5`}>Novars</h1>
                     <p className=' font-body mb-10'>We ensure your garden security when you are at home or not. Wherever you are you can stay update on your house security and you can also monitor everything going on.</p>
                     <div className=' flex gap-5 text-3xl'>
                         {
@@ -61,7 +61,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className={` w-full footerColor  text-white flex justify-between px-10 py-5`}>
-                <p className=' font-body'>Copyright &copy; 2023 Nova</p>
+                <p className=' font-body'>Copyright &copy; 2023 Novars</p>
                 <p className=' font-body'>All Rights Reserved</p>
             </div>
         </div>
